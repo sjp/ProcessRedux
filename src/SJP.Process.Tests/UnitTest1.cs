d2a71ae0c5d7351088c6dff25c316f60d1735551
Simon Potter <simon@sjp.co.nz>
2017-08-17 21:40:48 -0400

    Add unit test project (which does nothing yet).

---
+++ src/SJP.Process.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace SJP.Process.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1)
+        }
+    }
+}
