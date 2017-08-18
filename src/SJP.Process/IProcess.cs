0c81455b94e38220ea75bdcfc37692e4e0a76e1d
Simon Potter <simon@sjp.co.nz>
2017-08-17 21:52:49 -0400

    Begin wrapping existing behaviour in an IProcess interface.
    Useful for testing but also for extending the existing behaviour (if
    needed) later.

---
+++ src/SJP.Process/IProcess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using SysProcess = System.Diagnostics.Process;
+using SysStartInfo = System.Diagnostics.ProcessStartInfo;
+
+namespace SJP.Process
+{
+    public interface IProcess
+    {
+        /// <summary>
+        /// Gets the base priority of the associated process.
+        /// </summary>
+        int BasePriority { get; }
+
+        /// <summary>
+        /// Gets or sets whether the <see cref="Exited"/> event should be raised when the process terminates.
+        /// </summary>
+        bool EnableRaisingEvents { get; }
+
+        /// <summary>
+        /// Occurs when a process exits.
+        /// </summary>
+        event EventHandler Exited;
+    }
+}
---
+++ src/SJP.Process/ProcessAdapter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using SysProcess = System.Diagnostics.Process;
+using SysStartInfo = System.Diagnostics.ProcessStartInfo;
+
+namespace SJP.Process
+{
+    public class ProcessAdapter : IProcess
+    {
+        public ProcessAdapter(SysProcess process)
+        {
+            _process = process ?? throw new ArgumentNullException(nameof(process));
+        }
+
+        public static implicit operator ProcessAdapter(SysProcess process) => new ProcessAdapter(process);
+
+        public event EventHandler Exited;
+
+        public int BasePriority => _process.BasePriority;
+
+        public bool EnableRaisingEvents => _process.EnableRaisingEvents;
+
+        private readonly SysProcess _process;
+    }
+}
